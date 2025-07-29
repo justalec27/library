22f8d3a293e0113cde25eb5fe22a77066e0d29c6
Alec <215294797+justalec27@users.noreply.github.com>
2025-07-29 13:26:10 +0200

    add new book button with form to submit book

--- script.js
+++ script.js
@@ -1,58 +1,8 @@
-/*
-<div class="book-card">
-                    <div class="inner-box">
-                        <p class="title">Title:</p>
-                        <p class="author">Author:</p>
-                        <p class="pages">Pages:</p>
-                        <p class="status">Status:</p>
-                        <p class="book-id">Book ID:</p>
-                    </div>
-                </div>
-
-
-
-*/
-
-
 
 const myLibrary = []
 
 const innerBox= document.querySelector(".bottom-section .inner-box")
 
-function createCard() {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
-    innerBox.appendChild(bookCard)
-    const cardInnerBox = document.createElement("div")
-    cardInnerBox.classList.add("inner-box")
-    bookCard.appendChild(cardInnerBox)
-    const title = document.createElement("p")
-    const node = document.createTextNode("Title")
-    bookCard.appendChild(title)
-    title.appendChild(node)
-    cardInnerBox.appendChild(title)
-    const author = document.createElement("p")
-    const node2 = document.createTextNode("Author")
-    bookCard.appendChild(author)
-    author.appendChild(node2)
-    cardInnerBox.appendChild(author)
-     const pages = document.createElement("p")
-    const node3 = document.createTextNode("Pages:")
-    bookCard.appendChild(pages)
-    pages.appendChild(node3)
-    cardInnerBox.appendChild(pages)
-     const status = document.createElement("p")
-    const node4 = document.createTextNode("Status:")
-    bookCard.appendChild(status)
-    status.appendChild(node4)
-    cardInnerBox.appendChild(status)
-     const bookId = document.createElement("p")
-    const node5 = document.createTextNode("Book ID:")
-    bookCard.appendChild(bookId)
-    bookId.appendChild(node5)
-    cardInnerBox.appendChild(bookId)
-
-}
 
 function Book(title, author, pages, read) {
   if (!new.target){
@@ -74,48 +24,55 @@ function addBookToLibrary(book, array) {
     array.push({[id] : object});
 }
 
-console.log(myLibrary);
+console.log(myLibrary); //check-in
 
 const harryPotter = new Book("Harry Potter", "J.K. Rowling", 598, "not read yet")
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
 const atomicHabits = new Book("Atomic Habits", "James Clear", 548, "already read")
+const atomicHabits2 = new Book("Atomic Habits", "James Clear", 548, "already read")
 
 
 addBookToLibrary(harryPotter, myLibrary)
 addBookToLibrary(theHobbit, myLibrary)
 addBookToLibrary(atomicHabits, myLibrary)
+addBookToLibrary(atomicHabits2, myLibrary)
 
 const books = document.querySelector(".books")
 
 for (let i of myLibrary){
     console.log(i);
     const value = Object.values(i)[0];
-    console.log(value.title)
-    console.log(value.author)
-    console.log(value.info());
-    const para = document.createElement("p")
+    console.log(value.title);  //check-in
+    console.log(value.author); //check-in
+    console.log(value.info()); //check-in
+    const para = document.createElement("p");
     const node = document.createTextNode(value.info());
-    para.appendChild(node)
-    books.appendChild(para)
+    para.appendChild(node);
+    books.appendChild(para);
 
 }
 
 
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector(".new-book");
+const closeButton = document.querySelector("dialog button");
+const submitButton = document.querySelector("submit-btn")
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
 
 
-
-/* NEXT STEPS:
-    The loop is working to get the titles from the array.
-    1. Make sure the data is printed on the UI
-    2. Create a box for the info
-    3. Look in to how to print the info on the screen.
-
-    UPDATE evening:
-    I created a function for the cards. Now I need to make the function created the correct content.
-    The function should take arguments from the loop.
-
-*/
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
 
 
+// "Submit" the details in the dialog
+submitButton.addEventListener("click", (event) => {
+event.preventDefault()
+})
 
 
